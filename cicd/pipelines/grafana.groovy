03de70e977f511f3f50b0869190fbd98e1bb23dc
Ady Buxton <ady.buxton@fujitsu.com>
2020-10-12 17:42:20 +0100

    Changed logins to use Jenkins credentials

--- cicd/pipelines/grafana.groovy
+++ cicd/pipelines/grafana.groovy
@@ -19,7 +19,7 @@ spec:
         cpu: "200m"  
 """) {
   node(POD_LABEL) {
-    git branch: 'main', url: 'https://github.com/adyb-fj-cns/grafana-dashboards.git'
+    git branch: 'main', url: 'https://github.com/adyb-fj-cns/grafana-dashboards'
 
     stage('convert'){
       container('grafonnet') {
@@ -38,17 +38,24 @@ spec:
 
     stage('upload'){
       container('grafonnet') {
-        sh '''
-          SCRIPT_PATH="dashboards-jsonnet"; \
-          for file in $SCRIPT_PATH/*.json; \
-          do \
-            DASHBOARD=$(jq . $file); \
-            curl -X POST \
-              -H 'Content-Type: application/json' \
-              -d "{\\\"dashboard\\\": $DASHBOARD, \\\"overwrite\\\": true}" \
-              "http://admin:password@grafana-ui:3000/api/dashboards/db";
-          done
-          '''
+        withCredentials([usernamePassword(
+          credentialsId: 'grafana', 
+          usernameVariable: 'GRAFANA_USERNAME', 
+          passwordVariable: 'GRAFANA_PASSWORD')]) {
+
+          sh '''
+            SCRIPT_PATH="dashboards-jsonnet"; \
+            for file in $SCRIPT_PATH/*.json; \
+            do \
+              DASHBOARD=$(jq . $file); \
+              curl -X POST \
+                -H 'Content-Type: application/json' \
+                -d "{\\\"dashboard\\\": $DASHBOARD, \\\"overwrite\\\": true}" \
+                "http://$GRAFANA_USERNAME:$GRAFANA_PASSWORD@grafana-ui:3000/api/dashboards/db";
+            done
+            '''
+            
+        }
       }
     }
   
