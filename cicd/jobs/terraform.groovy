0c490c25b3742b11f2005c25b04e965fe6a9aaa0
Ady Buxton <ady.buxton@fujitsu.com>
2020-10-12 18:39:37 +0100

    Archived old pipelines

--- cicd/jobs/terraform.groovy
+++ cicd/jobs/terraform.groovy
@@ -1,17 +1,58 @@
-pipelineJob('terraform-job') {
-    displayName('Terraform job')
+podTemplate(yaml: """
+apiVersion: v1
+kind: Pod
+metadata:
+  name: terraform-pod
+spec:
+  containers:
+  - name: terraform
+    image: hashicorp/terraform:light
+    command:
+    - cat
+    tty: true
+    resources:
+      requests:
+        memory: "64Mi"
+        cpu: "250m"
+      limits:
+        memory: "128Mi"
+        cpu: "500m"
+""") {
+    node(POD_LABEL) {
+      git 'https://github.com/adyb-fj-cns/jenkins-terraform-example.git'
 
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/adyb-fj-cns/jenkins-example-pipelines.git')
-                    }
-                    branches('*/master')
-                }
-            }
-            scriptPath('cicd/pipelines/terraform.groovy')
+      stage('init'){
+        container('terraform') {
+          sh """
+            terraform init \
+              -backend-config="address=consul-ui" \
+              -input=false
+            """
         }
+      }
+
+      stage('plan'){
+        container('terraform') {
+          sh """
+            terraform plan \
+              -var-file=jenkins.tfvars \
+              -out=tfplan \
+              -input=false
+            """
+        }
+      }
+
+      stage('apply'){
+        container('terraform') {
+          sh """
+            terraform apply \
+              -input=false \
+              tfplan
+
+            cat message.txt
+
+            """
+        }
+      }  
     }
 }
