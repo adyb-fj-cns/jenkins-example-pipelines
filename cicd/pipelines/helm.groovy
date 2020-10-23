ddc69ab519b1a37706cd2703a01a81fe81cac46b
Ady Buxton <61878114+adyb-fj-cns@users.noreply.github.com>
2020-10-23 21:00:28 +0100

    Update helm.groovy

--- cicd/pipelines/helm.groovy
+++ cicd/pipelines/helm.groovy
@@ -4,7 +4,7 @@ kind: Pod
 spec:
   containers:
   - name: k8s
-    image: dtzar/helm-kubectl:3.3.4
+    image: adybfjcns/helm-kubectl-envsubst
     command:
     - cat
     tty: true
@@ -17,15 +17,22 @@ spec:
         cpu: "200m" 
 """) {
     node(POD_LABEL) {
+        
+        git branch: 'main', url: 'https://github.com/adyb-fj-cns/demo-helm-values.git'
+         
         container('k8s') {
-            withCredentials([kubeconfigContent(credentialsId: 'kubeconfig', variable: 'KUBECONFIG_FILE')]) {
+            withCredentials([kubeconfigContent(credentialsId: 'do-fra1-finx-dev', variable: 'KUBECONFIG_FILE')]) {
                 sh """
                     set +x
                     echo "$KUBECONFIG_FILE" >> kubeconfig
                     set -x
-                    kubectl get pods --kubeconfig kubeconfig
-                    kubectl get nodes --kubeconfig kubeconfig
-                    helm list --kubeconfig kubeconfig 
+                    helm repo add finx https://chartmuseum.157.230.76.69.nip.io --username adybuxton --password password
+                    
+                    export REPLICA_COUNT=1
+                    export TAG=latest
+                    envsubst < "values.tpl" > "jenkins-values.yaml"
+                    
+                    helm upgrade demo finx/demo -f jenkins-values.yaml --kubeconfig kubeconfig
                 """
             }
         }
