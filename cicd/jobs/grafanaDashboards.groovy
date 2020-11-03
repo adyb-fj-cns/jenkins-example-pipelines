af6ac6de2978228a58000073257b6e61c9313725
Ady Buxton <ady.buxton@fujitsu.com>
2020-11-03 13:32:22 +0000

    Updated jobs

--- cicd/jobs/grafanaDashboards.groovy
+++ cicd/jobs/grafanaDashboards.groovy
@@ -1,3 +1,44 @@
+//Based on https://gitlab.gnome.org/World/gimp-ci/jenkins-dsl/-/blob/master/jobs/gimp_multibranch_pipelines.groovy
+import jenkins.model.Jenkins
+
+def generatePipelineJob(String jobName, Map settings) {
+    multibranchPipelineJob(jobName) {
+        displayName(settings['displayName'])
+        branchSources {
+            branchSource {
+                source {
+                    git {
+                        id 'git-scm'
+                        remote settings['remote']
+                        traits {
+                            gitBranchDiscovery()
+                            gitTagDiscovery()
+                        }
+                    }
+                }
+            }
+        }
+        triggers {
+            cron(settings['cron'])
+        }
+    }
+}
+
+
+Map multibranch_jobs = [
+    grafanaDashboards: [
+        name: 'grafanaDashboards',
+        displayName: 'Convert and upload Grafana Dashboards (Multibranch)',
+        remote: 'https://github.com/adyb-fj-cns/grafana-dashboards.git', 
+        cron: 'H H * * *'
+    ]
+]
+
+multibranch_jobs.each { String name, Map settings ->
+    generatePipelineJob name, settings
+}
+
+/*
 multibranchPipelineJob('grafanaDashboards') {
     displayName('Convert and upload Grafana Dashboards (Multibranch)')
 
@@ -18,5 +59,5 @@ multibranchPipelineJob('grafanaDashboards') {
     triggers {
         cron('H H * * *')
     }
-
 }
+*/
